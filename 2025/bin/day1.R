7ad576f5398d98182db8ed3aee2d09d6002b403c
andreyhgl <andrey.hoglund@gmail.com>
2025-12-06 10:12:03 +0100

    day1 first puzzle

--- 2025/bin/day1.R
+++ 2025/bin/day1.R
@@ -1,5 +1,43 @@
+#!/usr/bin/env Rscript
+
 args <- commandArgs(trailingOnly=T)
 
 txt <- readLines(args[1], warn=F)
 
-print(txt)
+#rotation <- substr(txt, 1, 1)
+#number <- substr(txt, 2, 100)
+
+pos <- 50
+counter <- 0
+counter2 <- 0
+for (value in txt){
+	rotation <- substr(value, 1, 1)
+	number <- as.numeric(substr(value, 2, 100))
+
+	if (number > 100) counter2 <- counter2 + floor(number / 100)
+
+	if (rotation == "L") pos <- pos - number %% 100
+	if (rotation == "R") pos <- pos + number %% 100
+	pass <- "false"
+	if (pos == 0 || pos == 100) counter <- counter + 1
+
+	if (pos < 0){
+		if (number > 1) counter2 <- counter2 + 1
+		pos <- pos + 100
+
+		#print(pos)
+		pass <- "true"
+	}
+	if (pos > 100){
+		pos <- pos - 100
+		counter2 <- counter2 + 1
+		#print(pos)
+		pass <- "true"
+	}
+	print(c(value, pos, pass))
+
+}
+print(counter) # 1129
+print(counter2)
+print(counter2 + counter) # 7205 wrong
+#print(txt)
